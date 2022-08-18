9c0877536e5137b7eb24f350b3deb33528b0fa01
Hillary Ameyaw Owusu <hillary.owusu@amalitech.org>
2022-08-18 09:53:17 -0700

    my first react project

---
+++ src/components/Stylesheet.js
@@ -0,0 +1,162 @@
+import React from "react";
+import './myStyle.css'
+
+const StyleSheet = () =>{
+     return(
+        <div className="MainPage">
+        <div className="Subpage1-main">
+            <div className="Subpage1">
+                <div className="Sub1-child">
+                <div className="Sub1-child1">
+                    <div className="child1-img"></div>
+                </div>
+                <div className="Sub1-child2">
+
+                    <div className="text1"><p>Des brises d'été ondulaient l'herbe verte. Le lac bleu étincelait. Près du lac, une maman canard était assise sur un nid qui </p></div>
+                    <div className="text2"><span>Le grand oeuf n'avait pas encore éclos.Le grand oeuf n'avait pas encore éclos.Le grand oeuf n'avait pas encore éclos Le grand oeuf n'avait pas encore éclos</span></div>
+                    <div className="text3"><p>"Je parie que c'est un oeuf de dinde," dit une grand'mère canard. "Les dindes ne savent pas nager — ne t'embête pas avec ca</p></div>
+                    <div className="text4"><p>peut-être es-tu une dinde! Voyons si tu peux nager.</p></div>
+
+                </div>
+                 <div className="Sub1-child3">
+                    <input type={'text'} placeholder={'tu peux nager'}></input>
+                    <input type={'text'} placeholder={' avec ca'}></input>
+                  <button>pas avec ca</button>
+                    <div className='calender'>
+                        <img src="./images/hill.png"></img>
+                        <p>Dia 12 de Novembro</p> <span>as 19h37</span>
+                    </div>
+                 </div>
+                 
+                </div>
+                <div className="Sub2-child">
+                    
+                </div>
+                </div>
+        </div>
+        <div className="subpage2-main">
+                    <div className="page2-child">
+                    <div className="page2-child1">
+                        <h2>O QUE VOCE VAI APRENDER</h2>
+                        </div>
+                        <div className="page2-child2">
+                        <div className="child2-sub1">
+                            <div className="firstdiv">
+                                <img src="./images/ken.png" width={'70px'} height={'70px'}></img>
+                            </div>
+                            <div  className="firstdiv1"><p>Des brises d'été ondulaient l'herbe verte. <strong> lac bleu étincelait.</strong>Le lac bleu étincelait. Près du lac, une maman canard était assise <br></br>sur un nid qui</p></div>
+                        </div>
+                        <div className="child2-sub2">
+
+                        <div className="seconddiv">
+                                <img src="./images/got.png" width={'70px'} height={'70px'}></img>
+                            </div>
+                            <div  className="seconddiv1">
+                                <p>
+                                Je parie que <strong>c'est un oeuf</strong> de dinde," dit une grand'mère canard. "Les dindes ne savent pas nager — ne t'embête <br></br>pas avec ca  
+                                </p>
+                            </div>
+                        </div>
+                        <div className="child2-sub3">
+                        <div className="thirddiv">
+                                <img src="./images/now.png" width={'70px'} height={'70px'}></img>
+                            </div>
+                            <div  className="thirddiv1"><p>"Venez avec moi," dit la maman canard, "je vous emmène <strong>ligne à travers</strong>  rencontrer la bande" Les petits canards nagèrent en le lac.  rencontrer les<br></br> nouveaux canetons.</p></div>
+                        </div>
+                        <div className="child2-sub4">
+                        <div className="fourthdiv">
+                                <img src="./images/she.png" width={'90px'} height={'70px'}></img>
+                            </div>
+                            <div  className="fourthdiv1"><p>Les canetons trempèrent leurs têtes et pagayèrent avec leurs pieds. Le grand caneton gris était <strong>capable de flotter comme</strong> un bateau et pagayer comme un champion.</p></div>
+                        </div>
+                    </div>
+                    <div  className="page2-child3">
+                        <button>QUERO PARTICIPAR</button>
+                    </div>
+                    </div>
+                    </div>
+                    <div className="subpage3-main">
+                        <div className="subpage3-main1">
+                        <div  className="subpage3-child1"><h2>ESTE EVENTO E OBIRIGATORIO PARA QUEM</h2></div>
+                        <div  className="subpage3-child2">
+                            <div className="subpage3-div1"><p>Et il fit ainsi, mais ce n'était pas facile. Le caneton rencontra des oiseaux sauvages et des chiens méchants. </p>         <h1>01</h1></div>
+                            <div className="subpage3-div2"><p>L'automne arriva. Un jour, le vilain caneton vit une bande d'oiseaux blancs. Ils planaient et tournaient dans le ciel. Le vilain caneton n'avait jamais vu de tels oiseaux avant.</p>
+                            <h1>02</h1>
+                            </div>
+                            <div className="subpage3-div3"><p>La neige commenca à tomber. Tous les lacs et marais gelèrent. Les vents d'hiver soufflaient. Le vilain caneton passa un moment difficile. Puis, les jours devinrent plus chauds. Le vilain caneton s'étirait  </p>         <h1>03</h1></div>
+                        </div>
+                        </div>
+                    </div>
+                    <div className="Subpage4-main">
+                    <div className="Subpage4-main1">
+<div className="Subpage4-child1">
+<div className="page4-child1">
+    <p>L'automne arriva. Un jour, le vilain caneton </p>
+    </div>
+    <div className="page4-child2">
+   <p>Le vilain caneton s'étirait sous le soleil de printemps. Il agitait ses grosses ailes. Bien vite, il volait.</p>
+    </div>
+    <div className="page4-child3">
+   <p>Des enfants jouaient <strong>dans le jardin. </strong>"Regarde! Un nouveau <strong>cygne est venu!"</strong>  dirent-ils. Le vilain caneton entendit les enfants et <strong> se regarda dans l'eau.</strong></p>
+
+    </div>
+    <div className="page4-child4">
+   <p>Les autres cygnes le saluèrent. "Tu ES le plus joli de tous les cygnes" dirent-ils. Le vilain caneton rougit. "Je ne suis pas le plus beau de tous les cygnes" </p>
+    </div>
+    <div className="page4-child5">
+        <p>   Mais ils volaient vers le sud pour l'hiver. Les gracieux oiseaux agitaient leurs énormes ailes en s'en allant et laissèrent le vilain caneton tout seul.</p>
+ 
+    </div>
+</div>
+<div className="Subpage4-child2">
+<img src="./images/most.png" width={'600px'} height={'524px'}></img>
+</div>
+</div>
+</div>
+                    <div className="Subpage5-main">
+<div className="Subpage5-child1">
+<h2>O EVENTO COMECA EM..</h2>
+</div>
+<div className="Subpage5-child2">
+<div className="page5-sub1">
+    <div className="numbers">
+        <h1>11</h1>
+    </div>
+    <div className="numbers1">    <h1>10</h1></div>
+    <div className="numbers2">    <h1>15</h1></div>
+    <div className="numbers4">    <h1>19</h1></div>
+</div>
+<div className="page5-sub2">
+<div className="days">
+        <p>Days</p>
+    </div>
+    <div className="days1">
+        <p>Hours</p>
+    </div>
+    <div className="days2">
+        <p>Minutes</p>
+    </div>
+    <div className="days3">
+        <p>Seconds</p>
+    </div>
+</div>
+
+</div>
+<div className="Subpage5-child3">
+
+                        <button>QUERO PARTICIPAR</button>
+                    
+</div>
+                    </div>
+                    <div className="submainpage6">
+                        <img src="./images/Powered by.png"></img>
+                        <img src="./images/Group 1 (4).png" ></img>
+                        <img src="./images/zuck.png"></img>
+                        <img src="./images/EXPERTS.png"></img>
+                        <img src="./images/TODOS DIREITOS RESERVADOS.png" width={'250px'} height={'13px'}></img>
+                    </div>
+                    
+        </div>
+     )
+}
+export default StyleSheet
